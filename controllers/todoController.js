f86a649b189b5ea2a5b3630e3257989059f7f586
l3sterb3sabe <besabelester@gmail.com>
2018-09-03 23:50:43 +0800

    Mongoose Added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,30 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connecting to database
+mongoose.connect('mongodb://test:test123@ds243502.mlab.com:43502/todo');
+
+//creating a schema
+var todoSchema = new mongoose.Schema({
+	item : String
+});
+//creating a model
+var Todo = mongoose.model('Todo', todoSchema);
+
+var item1 = Todo({item: 'Get a job'}).save(function(err){
+	if(err) throw err;
+	console.log('Item Saved');
+});
+
+var urlencodedParser = bodyParser.urlencoded({extended : false});
+
 module.exports= function(app){
 
 	app.get('/todo', function(req, res){
 		res.render('todo');
 	});
 
-	app.post('/todo', function(req, res){
+	app.post('/todo', urlencodedParser, function(req, res){
 		
 	});
 
